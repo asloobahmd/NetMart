485e38c571a3debf7053d998d05f5aaa2668f245
shriwa <shriwa3429@gmail.com>
2024-07-29 09:05:21 +0530

    Initial Update

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Heading } from "@chakra-ui/react";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+      {/* <SignUp /> */}
+      {/* <Login /> */}
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ Client/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Navbar_2 from "../Components/Navbar_2";
+import Hero_1 from "../Components/Home/Hero_1";
+import Hero_2 from "../Components/Home/Hero_2";
+import Footer from "../Components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <Navbar_2 />
+      {/* <Hero_1 /> */}
+      <Hero_2 />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
